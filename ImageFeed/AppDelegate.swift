760bb6554324bd023a380babf14270783dfb40eb
shatt21 <poller2224@yandex.ru>
2024-04-23 16:09:12 +0300

    Интерфейс таблицы

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -9,11 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        sleep(2)
         // Override point for customization after application launch.
         return true
     }
